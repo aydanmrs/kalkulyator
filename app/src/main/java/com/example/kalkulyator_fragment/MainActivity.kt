e019ff0649110a56e17f490a51217f20d2522563
aydanmrs <164398449+aydanmrs@users.noreply.github.com>
2024-08-09 19:38:38 +0400

    Status bar commit

--- app/src/main/java/com/example/kalkulyator_fragment/MainActivity.kt
+++ app/src/main/java/com/example/kalkulyator_fragment/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.kalkulyator_fragment
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.statusBarColor = Color.TRANSPARENT
+        }
     }
 }
